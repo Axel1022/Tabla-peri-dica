9b2550bc8c5fc03b350db774219762e0b2061b7f
Gary Alexander Campusano Paredes <Ingcampusano@Outlook.com>
2024-06-26 12:47:32 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const path = require("path");
+const homeRouter = require("./routers/home");
+const app = express();
+const puerto = 8080;
+
+
+const hbs = exphbs.create({
+  extname: ".hbs",
+  defaultLayout: false,
+});
+
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(homeRouter);
+
+app.use("/", function (req, res, next) {
+  res.status(404).render("404", { layout: false });
+});
+
+app.listen(puerto);
